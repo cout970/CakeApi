34a00284cf37fe906786b568094f9d4ea4c5fdb5
cout970 <thecout970@gmail.com>
2015-08-14 22:28:13 +0200

    test
    Signed-off-by: cout970 <thecout970@gmail.com>

--- main/test/com/cout970/testplugin/Plugin.java
+++ main/test/com/cout970/testplugin/Plugin.java
@@ -8,6 +8,7 @@ import org.cakepowered.api.event.EventSuscribe;
 import org.cakepowered.api.event.InitializationEvent;
 import org.cakepowered.api.event.PlayerChatEvent;
 import org.cakepowered.api.event.PlayerJoinEvent;
+import org.cakepowered.api.event.ServerStatingEvent;
 import org.cakepowered.api.scoreboard.Team;
 import org.cakepowered.api.util.text.TextFormating;
 
@@ -25,6 +26,13 @@ public class Plugin {
 		logger.info("debug plugin loaded Successful");
 		INSTANCE = this;
 	}
+	
+	@EventSuscribe
+	public void onStart(ServerStatingEvent event){
+		game.getWorldManager().createWorld(2);
+		game.getWorldManager().createWorld(3);
+	}
+	
 	
 	@EventSuscribe
 	public void onPlayerChat(PlayerChatEvent event){
