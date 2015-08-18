aa011deaa0567c0531a78512b6265f51db5a633a
cout970 <thecout970@gmail.com>
2015-08-18 17:49:00 +0200

    comments
    Signed-off-by: cout970 <thecout970@gmail.com>

--- main/api/org/cakepowered/api/base/EventRegistry.java
+++ main/api/org/cakepowered/api/base/EventRegistry.java
@@ -1,11 +1,19 @@
 package org.cakepowered.api.base;
 
 import org.cakepowered.api.event.Event;
+import org.cakepowered.api.event.EventSuscribe;
 
 public interface EventRegistry {
 
+	/**
+	 * Fires an event that will listened by any registered methods that have this event or other event that extends/implements this event as parameter
+	 */
 	public boolean postEvent(Event e);
 	
+	/**
+	 * Register an event listener
+	 * @param o The instance of a class with one or more methods with the annotation {@link EventSuscribe} and one event as parameter 
+	 */
 	public boolean registerEventListener(Object o);
 	
 }
