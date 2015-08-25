281d4c0e14f015b2229a5afeb4669e75658266c8
cout970 <thecout970@gmail.com>
2015-08-25 19:59:53 +0200

    fixed player teleportation
    Signed-off-by: cout970 <thecout970@gmail.com>

--- main/java/org/cakepowered/api/implement/ApiPlayer.java
+++ main/java/org/cakepowered/api/implement/ApiPlayer.java
@@ -16,23 +16,26 @@ import org.cakepowered.api.util.Title;
 import org.cakepowered.api.util.TitleUtils;
 import org.cakepowered.api.util.Vector3d;
 import org.cakepowered.api.util.text.TextFormating;
+import org.cakepowered.api.world.CustomTeleporter;
 
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.event.ClickEvent;
 import net.minecraft.event.ClickEvent.Action;
 import net.minecraft.scoreboard.ScorePlayerTeam;
+import net.minecraft.server.MinecraftServer;
 import net.minecraft.util.ChatComponentText;
 import net.minecraft.util.ChatStyle;
 import net.minecraft.util.IChatComponent;
 import net.minecraft.world.World;
+import net.minecraft.world.WorldServer;
 import net.minecraft.world.WorldSettings.GameType;
 
-public class ApiPlayer extends ApiEntity implements Player{
-	
+public class ApiPlayer extends ApiEntity implements Player {
+
 	protected EntityPlayer player;
 
-	public ApiPlayer(EntityPlayer player){
+	public ApiPlayer(EntityPlayer player) {
 		super(player);
 		this.player = player;
 	}
@@ -46,10 +49,10 @@ public class ApiPlayer extends ApiEntity implements Player{
 	public void sendMessage(String s) {
 		player.addChatComponentMessage(new ChatComponentText(s));
 	}
-	
+
 	@Override
-	public boolean equals(Object o){
-		if(o instanceof Player){
+	public boolean equals(Object o) {
+		if (o instanceof Player) {
 			return getUniqueID().equals(((Player) o).getUniqueID());
 		}
 		return false;
@@ -72,7 +75,7 @@ public class ApiPlayer extends ApiEntity implements Player{
 
 	@Override
 	public int getGameMode() {
-		return ((EntityPlayerMP)player).theItemInWorldManager.getGameType().getID();
+		return ((EntityPlayerMP) player).theItemInWorldManager.getGameType().getID();
 	}
 
 	@Override
@@ -83,8 +86,8 @@ public class ApiPlayer extends ApiEntity implements Player{
 
 	@Override
 	public boolean isOP() {
-		for(String s : ((ApiServer)CakeApiMod.server).server.getConfigurationManager().getOppedPlayerNames()){
-			if(getUserName().equals(s)){
+		for (String s : ((ApiServer) CakeApiMod.server).server.getConfigurationManager().getOppedPlayerNames()) {
+			if (getUserName().equals(s)) {
 				return true;
 			}
 		}
@@ -94,49 +97,62 @@ public class ApiPlayer extends ApiEntity implements Player{
 	@Override
 	public void setLocation(PreciseLocation loc) {
 		World w = player.getEntityWorld();
-		if(w.provider.getDimensionId() != loc.getDimension()){
-			player.travelToDimension(loc.getDimension());
+		if (w.provider.getDimensionId() != loc.getDimension()) {
+			teleportToDimension(player, loc);
 		}
 		player.setSpawnPoint(PluginInterface.getBlockPos(loc.getPosition()), true);
 		player.setPositionAndUpdate(loc.getX(), loc.getY(), loc.getZ());
-		((EntityPlayerMP)player).playerNetServerHandler.setPlayerLocation(loc.getX(), loc.getY(), loc.getZ(), loc.getYaw(), loc.getPitch());		
+		((EntityPlayerMP) player).playerNetServerHandler.setPlayerLocation(loc.getX(), loc.getY(), loc.getZ(),
+				loc.getYaw(), loc.getPitch());
 	}
-	
+
+	public static void teleportToDimension(EntityPlayer player, PreciseLocation loc) {
+		int oldDimension = player.worldObj.provider.getDimensionId();
+		EntityPlayerMP entityPlayerMP = (EntityPlayerMP) player;
+		WorldServer worldServer = MinecraftServer.getServer().worldServerForDimension(loc.getDimension());
+		MinecraftServer.getServer().getConfigurationManager().transferPlayerToDimension(entityPlayerMP,
+				loc.getDimension(), new CustomTeleporter(worldServer));
+		if (oldDimension == 1) {
+			player.setPositionAndUpdate(loc.getX(), loc.getY(), loc.getZ());
+			worldServer.spawnEntityInWorld(player);
+			worldServer.updateEntityWithOptionalForce(player, false);
+		}
+	}
+
 	@Override
 	public int getDirection() {
-		float yaw = wrapAngleTo180_float(player.rotationYaw);
-		if((yaw < 45 && yaw >= 0) || (yaw > -45 && yaw <= 0)){
+		float yaw = wrapTo180(player.rotationYaw);
+		if ((yaw < 45 && yaw >= 0) || (yaw > -45 && yaw <= 0)) {
 			return DirectionYaw.SOUTH;
-		} else if(yaw >= 45 && yaw < 135){
+		} else if (yaw >= 45 && yaw < 135) {
 			return DirectionYaw.WEST;
-		} else if(yaw <= -45 && yaw > -135){
+		} else if (yaw <= -45 && yaw > -135) {
 			return DirectionYaw.EAST;
-		} else if((yaw >= 135 && yaw <= 180) || (yaw <= -135 && yaw >= -180)){
+		} else if ((yaw >= 135 && yaw <= 180) || (yaw <= -135 && yaw >= -180)) {
 			return DirectionYaw.NORTH;
 		}
 		return 0;
 	}
-	
-	private static float wrapAngleTo180_float(float p_76142_0_){
-        p_76142_0_ %= 360.0F;
 
-        if (p_76142_0_ >= 180.0F)
-        {
-            p_76142_0_ -= 360.0F;
-        }
+	private static float wrapTo180(float angle) {
+		angle %= 360.0F;
 
-        if (p_76142_0_ < -180.0F)
-        {
-            p_76142_0_ += 360.0F;
-        }
+		if (angle >= 180.0F) {
+			angle -= 360.0F;
+		}
 
-        return p_76142_0_;
-    }
+		if (angle < -180.0F) {
+			angle += 360.0F;
+		}
+
+		return angle;
+	}
 
 	@Override
 	public void sendTitle(Title t) {
-		if(t == null)return;
-		TitleUtils.sendTitle(((EntityPlayerMP)player).playerNetServerHandler, player, t);
+		if (t == null)
+			return;
+		TitleUtils.sendTitle(((EntityPlayerMP) player).playerNetServerHandler, player, t);
 	}
 
 	@Override
@@ -160,14 +176,16 @@ public class ApiPlayer extends ApiEntity implements Player{
 
 	@Override
 	public void kick(String mes) {
-		if(mes == null) mes = "";
-		((EntityPlayerMP)entity).playerNetServerHandler.kickPlayerFromServer(mes);
+		if (mes == null)
+			mes = "";
+		((EntityPlayerMP) entity).playerNetServerHandler.kickPlayerFromServer(mes);
 	}
 
 	@Override
 	public void setPitchAndYaw(float p, float y) {
-		((EntityPlayerMP)player).playerNetServerHandler.setPlayerLocation(getLocation().getX(), getLocation().getY(), getLocation().getZ(), y, p);
-		
+		((EntityPlayerMP) player).playerNetServerHandler.setPlayerLocation(getLocation().getX(), getLocation().getY(),
+				getLocation().getZ(), y, p);
+
 	}
 
 	@Override
@@ -184,13 +202,13 @@ public class ApiPlayer extends ApiEntity implements Player{
 	public int getSelectedSlot() {
 		return player.inventory.currentItem;
 	}
-	
+
 	@Override
-	public boolean isSneaking(){
+	public boolean isSneaking() {
 		return player.isSneaking();
 	}
-	
-	public EntityPlayer getMcPlayer(){
+
+	public EntityPlayer getMcPlayer() {
 		return player;
 	}
 }
