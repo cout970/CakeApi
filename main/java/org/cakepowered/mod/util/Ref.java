5f276969f787699b2fecbe307cad7a97b608c1eb
cout970 <thecout970@gmail.com>
2015-09-26 00:58:20 +0200

    uploaded the first version on curseforge
    Signed-off-by: cout970 <thecout970@gmail.com>

--- main/java/org/cakepowered/mod/util/Ref.java
+++ main/java/org/cakepowered/mod/util/Ref.java
@@ -4,5 +4,5 @@ public class Ref {
 
 	public static final String MODID = "cakeapi";
 	public static final String MODNAME = "Cake Api";
-	public static final String VERSION = "0.1.0";
+	public static final String VERSION = "1.0.0";
 }
